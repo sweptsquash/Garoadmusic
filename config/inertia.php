defc0e7d3b5fe093bfbc1ab8d32671b72373e9e3
Stewart Marsh <sweptsquash@live.co.uk>
2024-06-02 16:47:37 +0100

    chore: update inertia ssr port

--- config/inertia.php
+++ config/inertia.php
@@ -20,7 +20,7 @@ return [
 
         'enabled' => true,
 
-        'url' => 'http://127.0.0.1:13714',
+        'url' => 'http://127.0.0.1:13715',
 
         'bundle' => base_path('bootstrap/ssr/ssr.js'),
 
--- resources/js/ssr.js
+++ resources/js/ssr.js
@@ -7,31 +7,34 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
 import { createSSRApp, h } from 'vue'
 import { ZiggyVue } from 'ziggy-js'
 
-createServer((page) =>
-    createInertiaApp({
-        page,
-        render: renderToString,
-        resolve: (name) =>
-            resolvePageComponent(`./pages/${name}.vue`, import.meta.glob('./pages/**/*.vue')).then(
-                (module) => {
+createServer(
+    (page) =>
+        createInertiaApp({
+            page,
+            render: renderToString,
+            resolve: (name) =>
+                resolvePageComponent(
+                    `./pages/${name}.vue`,
+                    import.meta.glob('./pages/**/*.vue'),
+                ).then((module) => {
                     const page = module.default
 
                     page.layout = page.layout || Layout
 
                     return page
-                },
-            ),
-        setup({ App, props, plugin }) {
-            globalThis.Ziggy = Ziggy
+                }),
+            setup({ App, props, plugin }) {
+                globalThis.Ziggy = Ziggy
 
-            return createSSRApp({
-                render: () => h(App, props),
-            })
-                .use(plugin)
-                .use(ZiggyVue, {
-                    ...Ziggy,
-                    location: page.props?.location ? new URL(page.props.location) : null,
+                return createSSRApp({
+                    render: () => h(App, props),
                 })
-        },
-    }),
+                    .use(plugin)
+                    .use(ZiggyVue, {
+                        ...Ziggy,
+                        location: page.props?.location ? new URL(page.props.location) : null,
+                    })
+            },
+        }),
+    13715,
 )
