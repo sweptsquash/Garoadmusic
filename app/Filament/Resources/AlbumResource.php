cfc298c1abca9d3e77ea36e702db5bba6e2705b1
Stewart Marsh <sweptsquash@live.co.uk>
2024-06-03 23:10:55 +0100

    feat: Filament base resources
    feat: Contact migration/model

---
+++ app/Filament/Resources/AlbumResource.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\AlbumResource\Pages;
+use App\Models\Album;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class AlbumResource extends Resource
+{
+    protected static ?string $model = Album::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListAlbums::route('/'),
+            'create' => Pages\CreateAlbum::route('/create'),
+            'edit' => Pages\EditAlbum::route('/{record}/edit'),
+        ];
+    }
+}
--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ContactStoreRequest;
 use App\Mail\ContactForm;
+use App\Models\Contact;
 use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
@@ -15,6 +16,8 @@ class ContactController extends Controller
 
     public function store(ContactStoreRequest $request)
     {
+        Contact::create($request->validated());
+
         Mail::to(config('services.contact_form.email'), config('services.contact_form.name'))
             ->send(new ContactForm($request->validated()));
 
